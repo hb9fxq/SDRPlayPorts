2c92897a92cacdac5960ea10e3ecb39c8ace1d39
Frank Werner-Krippendorf <mail@kripp.ch>
2016-04-08 22:56:19 +0200

    merge tools play_sdr/play_sdr16, fixes #1

--- play_sdr.c
+++ play_sdr.c
@@ -1,6 +1,7 @@
 /*
  * rtl-sdr, turns your Realtek RTL2832 based DVB dongle into a SDR receiver
  * Copyright (C) 2012 by Steve Markgraf <steve@steve-m.de>
+ * Copyright (C) 2012-2013 by Hoernchen <la@tfc-server.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,8 +16,30 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
+ * ************************************** THIS IS A FORK ******************* ORIGINAL COPYRIGHT SEE ABOVE.
  *
- * HB9FXQ, mail@hb9fxq.ch Frank Werner-Krippendorf (taken this file as is from https://github.com/SDRplay/examples)
+ *  SDRPlayPorts
+ *  Ports of some parts of rtl-sdr for the SDRPlay (original: git://git.osmocom.org/rtl-sdr.git /)
+ *  2016: Fork by HB9FXQ (Frank Werner-Krippendorf, mail@hb9fxq.ch)
+ *
+ *  Code changes I've done:
+ *  - removed rtl_sdr related calls and replaced them with mir_* to work with the SDRPlay
+ *  - removed various local variables
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
  *
  */
 
@@ -41,6 +64,11 @@
 #define DEFAULT_BUF_LENGTH		(336 * 2) // (16 * 16384)
 #define MINIMAL_BUF_LENGTH		672 // 512
 #define MAXIMAL_BUF_LENGTH		(256 * 16384)
+#define DEFAULT_LNA		        0;
+#define DEFAULT_GAIN_REDUCTION  0;
+#define DEFAULT_GAIN            40;
+#define DEFAULT_FREQUENCY       100000000;
+#define DEFAULT_RESULT_BITS     8; // more compatible with RTL_SDR
 
 static int do_exit = 0;
 static uint32_t bytes_to_read = 0;
@@ -49,6 +77,12 @@ short *ibuf;
 short *qbuf;
 unsigned int firstSample;
 int samplesPerPacket, grChanged, fsChanged, rfChanged;
+int resultBits = DEFAULT_RESULT_BITS;
+
+void adjsut_bw(int bwHz, mir_sdr_Bw_MHzT *ptr);
+void adjsut_if(int ifFreq, mir_sdr_If_kHzT *ptr);
+
+void adjsut_result_bits(int bits, int *ptrResultBits);
 
 double atofs(char *s)
 /* standard suffixes */
@@ -81,12 +115,15 @@ void usage(void)
 {
     fprintf(stderr,
             "play_sdr, an I/Q recorder for SDRplay RSP receivers\n\n"
-                    "Usage:\t -f frequency_to_tune_to [Hz]\n"
+                    "Usage:\t -f frequency_to_tune_to (Hz)\n"
                     "\t[-s samplerate (default: 2048000 Hz)]\n"
+                    "\t[-b Band Width in Hz (default: 1536) possible values: 200 300 600 1536 5000 6000 7000 8000]\n"
+                    "\t[-i IF in kHz (default: 0 (=Zero IF)) possible values: 0 450 1620 2048]\n"
                     "\t[-g gain (default: 50)]\n"
                     "\t[-n number of samples to read (default: 0, infinite)]\n"
-                    "\t[-R enable gain reduction (default: 0, disabled)]\n"
-                    "\t[-L RSP LNA enable (default: 0, disabled)]\n"
+                    "\t[-r enable gain reduction (default: 0, disabled)]\n"
+                    "\t[-l RSP LNA enable (default: 0, disabled)]\n"
+                    "\t[-x Result I/Q bit resolution (uint8 / short) (default: 0, possible values: 8 16)]\n"
                     "\tfilename (a '-' dumps samples to stdout)\n\n");
     exit(1);
 }
@@ -121,17 +158,22 @@ int main(int argc, char **argv)
     int n_read;
     mir_sdr_ErrT r;
     int opt;
-    int gain = 50;
+    int gain = DEFAULT_GAIN;
     FILE *file;
-    uint8_t *buffer;
-    uint32_t frequency = 100000000;
+
+    uint8_t *buffer8;
+    short *buffer16;
+
+    uint32_t frequency = DEFAULT_FREQUENCY;
     uint32_t samp_rate = DEFAULT_SAMPLE_RATE;
     uint32_t out_block_size = DEFAULT_BUF_LENGTH;
-    int rspMode = 0;
-    int rspLNA = 0;
+    int gainReductionMode = DEFAULT_GAIN_REDUCTION;
+    int rspLNA = DEFAULT_LNA;
     int i, j;
+    mir_sdr_Bw_MHzT bandwidth = mir_sdr_BW_1_536;
+    mir_sdr_If_kHzT ifKhz = mir_sdr_IF_Zero;
 
-    while ((opt = getopt(argc, argv, "f:g:s:n:r:l")) != -1) {
+    while ((opt = getopt(argc, argv, "f:g:s:n:r:l:b:i:x:")) != -1) {
         switch (opt) {
             case 'f':
                 frequency = (uint32_t)atofs(optarg);
@@ -146,10 +188,19 @@ int main(int argc, char **argv)
                 bytes_to_read = (uint32_t)atofs(optarg) * 2;
                 break;
             case 'r':
-                rspMode = atoi(optarg);
+                gainReductionMode = atoi(optarg);
                 break;
             case 'l':
                 rspLNA = atoi(optarg);
+                break;
+            case 'b':
+                adjsut_bw(atoi(optarg), &bandwidth);
+                break;
+            case 'i':
+                adjsut_if(atoi(optarg), &ifKhz);
+                break;
+            case 'x':
+                adjsut_result_bits(atoi(optarg), &resultBits);
                 break;
             default:
                 usage();
@@ -163,6 +214,18 @@ int main(int argc, char **argv)
         filename = argv[optind];
     }
 
+    fprintf(stdout, "[DEBUG] *************** play_sdr16 init summary *********************\n");
+    fprintf(stdout, "[DEBUG] LNA: %d\n", rspLNA);
+    fprintf(stdout, "[DEBUG] gainReductionMode: %d\n", gainReductionMode);
+    fprintf(stdout, "[DEBUG] samp_rate: %d\n", samp_rate);
+    fprintf(stdout, "[DEBUG] gain: %d\n", gain);
+    fprintf(stdout, "[DEBUG] frequency: [Hz] %d / [MHz] %f\n", frequency, frequency/1e6);
+    fprintf(stdout, "[DEBUG] bandwidth: [kHz] %d\n", bandwidth);
+    fprintf(stdout, "[DEBUG] IF: %d\n", ifKhz);
+    fprintf(stdout, "[DEBUG] Result I/Q bit resolution (bit): %d\n", resultBits);
+    fprintf(stdout, "[DEBUG] *************************************************************\n");
+
+
     if(out_block_size < MINIMAL_BUF_LENGTH ||
        out_block_size > MAXIMAL_BUF_LENGTH ){
         fprintf(stderr,
@@ -174,15 +237,23 @@ int main(int argc, char **argv)
         out_block_size = DEFAULT_BUF_LENGTH;
     }
 
-    buffer = malloc(out_block_size * sizeof(uint8_t));
+    if(resultBits == 8) {
+        buffer8 = malloc(out_block_size * sizeof(uint8_t));
+    }
+    else {
+        buffer16 = malloc(out_block_size * sizeof(short));
+    }
 
     r = mir_sdr_Init(40, 2.0, 100.00, mir_sdr_BW_1_536, mir_sdr_IF_Zero,
                      &samplesPerPacket);
+
     if (r != mir_sdr_Success) {
         fprintf(stderr, "Failed to open SDRplay RSP device.\n");
         exit(1);
     }
+
     mir_sdr_Uninit();
+
 #ifndef _WIN32
     sigact.sa_handler = sighandler;
     sigemptyset(&sigact.sa_mask);
@@ -208,9 +279,10 @@ int main(int argc, char **argv)
         }
     }
 
-    if (rspMode == 1)
+    if (gainReductionMode == 1)
     {
         mir_sdr_SetParam(201,1);
+
         if (rspLNA == 1)
         {
             mir_sdr_SetParam(202,0);
@@ -220,12 +292,12 @@ int main(int argc, char **argv)
             mir_sdr_SetParam(202,1);
         }
         r = mir_sdr_Init(gain, (samp_rate/1e6), (frequency/1e6),
-                         mir_sdr_BW_1_536, mir_sdr_IF_Zero, &samplesPerPacket );
+                         bandwidth, ifKhz, &samplesPerPacket );
     }
     else
     {
         r = mir_sdr_Init((78-gain), (samp_rate/1e6), (frequency/1e6),
-                         mir_sdr_BW_1_536, mir_sdr_IF_Zero, &samplesPerPacket );
+                         bandwidth, ifKhz, &samplesPerPacket );
     }
 
     if (r != mir_sdr_Success) {
@@ -251,18 +323,24 @@ int main(int argc, char **argv)
         j = 0;
         for (i=0; i < samplesPerPacket; i++)
         {
-            buffer[j++] = (unsigned char) (ibuf[i] >> 8);
-            buffer[j++] = (unsigned char) (qbuf[i] >> 8);
+            if(resultBits == 8){
+                buffer8[j++] = (unsigned char) (ibuf[i] >> 8);
+                buffer8[j++] = (unsigned char) (qbuf[i] >> 8);
+            }
+            else {
+                buffer16[j++] = ibuf[i];
+                buffer16[j++] = qbuf[i];
+            }
         }
 
-        n_read = (samplesPerPacket * 2);
+        n_read = (samplesPerPacket * (resultBits / 4));
 
         if ((bytes_to_read > 0) && (bytes_to_read <= (uint32_t)n_read)) {
             n_read = bytes_to_read;
             do_exit = 1;
         }
 
-        if (fwrite(buffer, 1, n_read, file) != (size_t)n_read) {
+        if (fwrite(resultBits == 8 ? buffer8 : buffer16, 1, n_read, file) != (size_t)n_read) {
             fprintf(stderr, "Short write, samples lost, exiting!\n"); break;
         }
 
@@ -284,7 +362,52 @@ int main(int argc, char **argv)
         fclose(file);
 
     mir_sdr_Uninit();
-    free (buffer);
+    free (resultBits == 8 ? buffer8 : buffer16);
     out:
     return r >= 0 ? r : -r;
+}
+
+void adjsut_result_bits(int bits, int *ptrResultBits) {
+    switch (bits){
+        case 8:
+        case 16:
+            *ptrResultBits = bits;
+            return;
+    }
+
+    fprintf(stderr, "Invalid result I/Q resolution (-x) !\n");
+    usage();
+}
+
+void adjsut_if(int ifFreq, mir_sdr_If_kHzT *ptrIFKhz) {
+    switch (ifFreq){
+        case 0:
+        case 450:
+        case 1620:
+        case 2048:
+            *ptrIFKhz = ifFreq;
+            return;
+    }
+
+    fprintf(stderr, "Invalid IF frequency (-i) !\n");
+    usage();
+
+}
+
+void adjsut_bw(int bwHz, mir_sdr_Bw_MHzT *ptrBw) {
+
+    switch (bwHz){
+        case 200:
+        case 300:
+        case 600:
+        case 1536:
+        case 5000:
+        case 6000:
+        case 8000:
+            *ptrBw = bwHz;
+            return;
+    }
+
+    fprintf(stderr, "Invalid Band Width (-b) !\n");
+    usage();
 }
